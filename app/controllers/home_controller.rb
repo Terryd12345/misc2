48fa053aa82585680fa4928d95f94b58d5a0f60d
Terence <terrencedenning@gmail.com>
2018-07-12 14:51:35 +1000

    some style changes to homepage and added member section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs
+  get '/about', to: 'home#about'
   root to: "home#index"
 end
